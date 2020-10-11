5046273f075abf1aa60bbf38c15afb8bcf073031
lendoo <lendoo73@gmail.com>
2020-10-11 20:07:54 +0100

    Create prob_calculator.py

---
+++ Python/Scientific Computing_with_Python/Probability_Calculator/prob_calculator.py
@@ -0,0 +1,62 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+class Hat:
+  
+  def __init__(self, **balls):
+    self.contents = self.create_contents(**balls)
+
+  # methods:
+  def create_contents(self, **balls):
+    contents = []
+    for color, quantity  in balls.items():
+      group = [color] * quantity
+      contents.extend(group)
+    return contents
+
+  def draw(self, num):
+    if num >= len(self.contents):
+      # cannot copy a list simply by typing list2 = list1, because: list2 will only be a reference to list1, and changes made in list1 will automatically also be made in list2.
+      result = list(self.contents)
+    else:
+      result = random.sample(self.contents, k = num)
+
+    # This method should remove balls at random from contents... Why need to remove it???
+    self.remove_balls(result)
+    # If the number of balls to draw exceeds the available quantity, return all the balls. ???
+    # Both methods unnecessary!
+
+    return result
+  
+  def remove_balls(self, balls):
+    for i in range(len(balls)):
+      self.contents.remove(balls[i])
+    
+  
+  def return_balls(self, balls):
+    self.contents.extend(balls)
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  M = 0
+
+  def check_sample(expected, sample):
+    for ball in expected:
+      if ball in sample:
+        sample.remove(ball)
+      else:
+        return False
+    return True
+      
+  for i in range(num_experiments):
+    # inicialize the expected list:
+    expected = hat.create_contents(**expected_balls)
+    sample = hat.draw(num_balls_drawn)
+    # If the number of balls to draw exceeds the available quantity, return all the balls. ???
+    hat.return_balls(sample)
+    if check_sample(expected, sample):
+      M += 1
+
+  return M / num_experiments
+
+
