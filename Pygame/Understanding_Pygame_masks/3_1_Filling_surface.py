8b9aaba6b7a44ac2a5d31b8fddc08f05f1b7ccbc
Csaba Cselko <lendoo73@gmail.com>
2022-08-28 19:54:46 +0100

    masks in pygame

---
+++ Pygame/Understanding_Pygame_masks/3_1_Filling_surface.py
@@ -0,0 +1,35 @@
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+
+# creating an obstacle
+obstacle_surf = pygame.image.load("alpha.png").convert_alpha()
+obstacle_pos = (100, 100)
+obstacle_mask = pygame.mask.from_surface(obstacle_surf)
+
+# mask back into a surface
+new_obstacle_surf = obstacle_mask.to_surface()
+new_obstacle_surf.set_colorkey((0, 0, 0))
+
+# filling in the surface with a color
+surf_w, surf_h = new_obstacle_surf.get_size()
+for x in range(surf_w):
+    for y in range(surf_h):
+        if new_obstacle_surf.get_at((x, y))[0] != 0:
+            new_obstacle_surf.set_at((x, y), "orange")
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill("grey")
+    screen.blit(obstacle_surf, obstacle_pos)
+    screen.blit(new_obstacle_surf, obstacle_pos)
+
+    pygame.display.update()
+    clock.tick(60)
